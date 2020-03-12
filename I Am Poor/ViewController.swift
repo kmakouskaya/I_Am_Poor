8805ec428a65de24783fae69f1ed93c2a8351c21
Kate Poppy <k.makouskaya@gmail.com>
2020-03-13 00:49:20 +0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Kate on 3/13/20.
+//  Copyright © 2020 k_poppy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
